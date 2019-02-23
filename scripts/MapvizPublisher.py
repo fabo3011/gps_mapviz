ea427c09a1cb9fa76a19b3dc463b651f70f7e285
AlexisGar <a01703422@itesm.mx>
2019-02-23 00:08:56 -0600

    Add config file with marker to /gpstarget

--- scripts/MapvizPublisher.py
+++ scripts/MapvizPublisher.py
@@ -25,6 +25,7 @@ def GpsMapper():
     pub = rospy.Publisher('GPS', GPSFix, queue_size=10)
     rospy.init_node('GPSMapviz', anonymous=True)
     rospy.Subscriber(listener_nodo,Twist,callback)
+    rospy.Subscriber(listener_nodo,Twist,callback)
 
     while not rospy.is_shutdown():
         if dataChanged:
